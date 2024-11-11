6958ae760aba326d13113ce618e6857145d5bedf
Rohan <lolcactusman2@gmail.com>
2024-11-11 21:57:09 +0000

    Simplify code

--- ags/.config/ags/options/desktop.ts
+++ ags/.config/ags/options/desktop.ts
@@ -1,21 +1,25 @@
 import { DesktopOptions, DesktopShortcutMenus } from "types";
+import apps from "./apps";
+
+function app(name: string, script: string): DesktopShortcutMenus {
+    return {
+        name,
+        script,
+        separator: false,
+    };
+}
+
+const separator: DesktopShortcutMenus = {
+    name: "",
+    script: "",
+    separator: true,
+};
 
 const shortcuts: DesktopShortcutMenus[] = [
-    {
-        name: "Colour Picker",
-        script: "hyprpicker -a",
-        separator: false,
-    },
-    {
-        name: "",
-        script: "",
-        separator: true,
-    },
-    {
-        name: "Codium",
-        script: "codium --ozone-platform-hint=auto --ozone-platform=wayland",
-        separator: false,
-    }
+    app("Terminal", apps.terminal.name),
+    app("Codium", "codium --ozone-platform-hint=auto --ozone-platform=wayland"),
+    separator,
+    app("Colour Picker", "hyprpicker -a"),
 ];
 
 export default {
